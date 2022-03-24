71f6d251e0d61950b8f366d4ee9551f1170f0450
EricKugel <erickugel713@gmail.com>
2022-03-24 11:25:58 -0500

    Finished 7.4 lab

--- Chapter-7/IsosTrapezoid.java
+++ Chapter-7/IsosTrapezoid.java
@@ -4,16 +4,28 @@ public class IsosTrapezoid extends Trapezoid {
     }
 
     public double calcDiagonal() {
-        return Math.hypot(getA(), getB());
+        return Math.hypot((getA() + (getC() - getA()) / 2), getE());
     }
 
     @Override
-    public boolean equals(Rectangle other) {
-        
+    public boolean equals(Object obj) {
+        IsosTrapezoid other = (IsosTrapezoid) obj;
+        if (getA() != other.getA() 
+            || getB() != other.getB()
+            || getC() != other.getC()
+            || getD() != other.getD()
+            || getE() != other.getE()
+        ) {
+            return false;
+        }
+        return true;
     }
 
     @Override
     public String toString() {
-        
+        String output = "Isosceles Trapezoid: ";
+        output += "Bases: " + getA() + ", " + getC() + "\n";
+        output += "Height: " + getE() + ", Legs: " + getB();
+        return output;
     }
 }
--- Chapter-7/Rectangle.java
+++ Chapter-7/Rectangle.java
@@ -6,4 +6,18 @@ public class Rectangle extends Parallelogram {
     public double calcDiagonal() {
         return Math.hypot(getA(), getB());
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        Rectangle other = (Rectangle) obj;
+        if ((getA() == other.getA() && getB() == other.getB()) || (getA() == other.getB() && getB() == other.getA())) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Rectangle: Base: " + getA() + ", Height: " + getB();
+    }
 }
