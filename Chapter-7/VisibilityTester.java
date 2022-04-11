9b538f42e74ae851a341bf6932d2d9f214f1e2c0
EricKugel <erickugel713@gmail.com>
2022-04-11 11:43:53 -0500

    Testing something

---
+++ Chapter-7/VisibilityTester.java
@@ -0,0 +1,27 @@
+public class VisibilityTester {
+    private int var1;
+    private String var2;
+
+    public VisibilityTester(int v1, String v2) {
+        var1 = v1;
+        var2 = v2;
+    }
+
+    public boolean equals(Object other) {
+        if (other == null) {
+            return false;
+        }
+        VisibilityTester s = (VisibilityTester) other;
+        return (var1 == s.var1
+            && var1 == var2.length()
+            && var2.length() == s.var2.length());
+    }
+    
+    public static void main(String[] arg0) {
+        VisibilityTester t1 = new VisibilityTester(3, "abdd");
+        VisibilityTester t2 = new VisibilityTester(4, "asdfja");
+        System.out.println(t1.equals(t2));
+    }
+}
+    
+
